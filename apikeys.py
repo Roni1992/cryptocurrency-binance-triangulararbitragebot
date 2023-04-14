b83e5d66c7bf46e88d3d3c16c6d99701acc0ab18
Roni1992 <roni39@mail.ru>
2023-04-15 00:55:47 +0300

    udate
    del api key

--- apikeys.py
+++ apikeys.py
@@ -1,2 +1,1 @@
-BinanceKey = {'api_key': '2miRMgNpYqD3Gy9D0YAzbMhINXNIeCqIZVf573faMk0mqEfp21vpal0V616oXCt5',
-               'api_secret': 'aNMBhV6fbObIsldOZukNxYb2rFVWr6607tB6l6jHt1KUiI6Zj1TJ2yulraTZ9YkX'}
+BinanceKey = {'api_key': }
