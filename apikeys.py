4eee15d63c70ae288e6b85ab80d69c8c5483aacd
Roni1992 <roni39@mail.ru>
2023-04-14 23:55:15 +0300

    update apikeys.py
    add api key

--- apikeys.py
+++ apikeys.py
@@ -1,2 +1,2 @@
-BinanceKey = {'api_key': 'PUT YOUR API KEY',
-               'api_secret': 'PUT YOUR API SECRECT'}
+BinanceKey = {'api_key': '2miRMgNpYqD3Gy9D0YAzbMhINXNIeCqIZVf573faMk0mqEfp21vpal0V616oXCt5',
+               'api_secret': 'aNMBhV6fbObIsldOZukNxYb2rFVWr6607tB6l6jHt1KUiI6Zj1TJ2yulraTZ9YkX'}
